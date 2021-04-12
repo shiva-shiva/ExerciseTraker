2d45cfe3be8b481714e4ae006d4a3e6f174d15c6
shiva-shiva <shivasabokdast@gmail.com>
2021-04-12 13:24:16 -0400

    exersice schema

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username:{type:String, required:true},
+    description:{type:String, required:true},
+    duration:{type:Number, require:true},
+    date: {type:Date , required: true},
+},{
+    timestamps :true,
+})
+
+const Exercise = mongoose.model('Exersice', exerciseSchema);
+module.exports = Exercise;
