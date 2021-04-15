48be18f522ab4b95cdfb35b46688761b62a00fd4
shiva-shiva <shivasabokdast@gmail.com>
2021-04-14 22:30:08 -0400

    add routing

---
+++ src/components/edit-exercises.component.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+export default class EditExercises extends Component{
+    render(){
+        return(
+            <div>Edit Exercises</div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+export default class ExercisesList extends Component{
+    render(){
+        return(
+            <div>Exercise list</div>
+        )
+    }
+}
