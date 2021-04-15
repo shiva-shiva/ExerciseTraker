cab3e57c999d592f7e3530a9c67165d81409b6f7
shiva-shiva <shivasabokdast@gmail.com>
2021-04-14 22:42:21 -0400

    add onChange method

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
+      <div className="container">
         <Navbar/>
          <br/>
          <Route path="/" exact component={ExercisesList}/>
          <Route path="/edit/:id" exact component={EditExercise}/>
          <Route path="/create" exact component={CreateExercise}/>
          <Route path="/user" exact component={CreateUser}/>
+         </div>
     </Router>
 
   );
--- src/components/create-exercises.component.js
+++ src/components/create-exercises.component.js
@@ -1,5 +1,38 @@
 import React, {Component} from 'react';
 export default class CreateExercise extends Component{
+     constructor(props){
+         super(props);
+        this.state ={
+            username:'',
+            description:'',
+            duration:0,
+            date:new Date(),
+            users:[]
+        }   
+     }
+     
+     onChangeUsername(e){
+         this.setState({
+            username: e.target.value,
+         })
+     }
+     onChangeDescription(e){
+        this.setState({
+            description: e.target.value,
+        })
+    }
+    onChangeDuration(e){
+        this.setState({
+            duration: e.target.value,
+        })
+    }
+    onChangeDate(date){
+        this.setState({
+            date: date,
+        })
+    }
+
+
     render(){
         return(
             <div>Create Exercises</div>
