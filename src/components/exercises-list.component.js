77127f8b3b32027f7bdbe724007a519bd3f5fce7
shiva-shiva <shivasabokdast@gmail.com>
2021-04-15 11:25:39 -0400

    delete exercises

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,30 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 export default class ExercisesList extends Component{
+    constructor(props){
+        super(props);
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state ={exercises :[]};
+    }
+
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/exercises/')
+        .then(response =>{
+           this.setState({exercises:response.data})
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    deleteExercise(id){
+        axios.delete('http://localhost:5000/exercises/'+id)
+        .then(res => console.log(res.data));
+        this.setState({
+            exercises:this.state.exercises.filter(el =>el._id !== id)
+        })
+    }
     render(){
         return(
             <div>Exercise list</div>
