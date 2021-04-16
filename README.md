d19a507678465d58e51b9f0552f209dcffc07c4d
shiva-shiva <shivasabokdast@gmail.com>
2021-04-16 15:57:57 -0400

    read me

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# ExerciseTracker
+
+
+
+<h1 align="center"> Exercise Tracker👋</h1>
+
+![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/shiva-shiva/readmeGenerator?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/shiva-shiva/readmeGenerator?style=flat&logo=appveyor)
+![Badge for GitHub licence](https://img.shields.io/github/license/shiva-shiva/readmeGenerator?style=flat&logo=appveyor)
+
+
+## Description 
+
+
+## ✨Demo
+
+![Demo](./public/exercise.gif)
+
+
+ ## Table of Contents
+* [Description](#Description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [license](#license)
+
+## Installation
+*Steps required to install project and how to get the development environment running:*
+
+    1- Clone this repository to your local machine.
+
+    2- Run npm install to install dependencies required for this project.
+
+
+## 💻usage
+*Instructions and examples for use:*</br> 
+
+    Run npm start to start the application.
+    To access the app after starting the server, go to http://localhost:3000 in your browser.
+    
+## Features
+
+    React
+    JavaScript library 
+    express
+    mongoose
+    mongodb
+
+## 📝License
+MIT License<br/>
+       ![Badge for GitHub licence](https://img.shields.io/github/license/shiva-shiva/readmeGenerator?style=flat&logo=appveyor)
+
+## Questions
+<br/>:octocat: Find me on GitHub:[shiva-shiva](https://github.com/shiva-shiva)<br />
+    <br />
+    ✉️ Email me with any questions: shivasabokdast@gmail.com<br /><br />
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import '../App.css'
 
 const Exercise = props => (
-  <tr>
-    <td>{props.exercise.username}</td>
-    <td>{props.exercise.description}</td>
-    <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
-    <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
-    </td>
-  </tr>
+  <li class="table-row">
+    <div class="col col-2" data-label="Job Id">{props.exercise.username}</div>
+    <div class="col col-3" data-label="Customer Name">{props.exercise.description}</div>
+    <div class="col col-2" data-label="Amount">{props.exercise.duration}</div>
+    <div class="col col-2" data-label="Payment Status">{props.exercise.date.substring(0, 10)}</div>
+    <div class="col col-2" data-label="Payment Status"> <Link to={"/edit/" + props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a></div>
+  </li>
 )
 
 export default class ExercisesList extends Component {
@@ -20,7 +19,7 @@ export default class ExercisesList extends Component {
 
     this.deleteExercise = this.deleteExercise.bind(this)
 
-    this.state = {exercises: []};
+    this.state = { exercises: [] };
   }
 
   componentDidMount() {
@@ -34,8 +33,8 @@ export default class ExercisesList extends Component {
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
-      .then(response => { console.log(response.data)});
+    axios.delete('http://localhost:5000/exercises/' + id)
+      .then(response => { console.log(response.data) });
 
     this.setState({
       exercises: this.state.exercises.filter(el => el._id !== id)
@@ -44,29 +43,25 @@ export default class ExercisesList extends Component {
 
   exerciseList() {
     return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
     })
   }
 
   render() {
     return (
-      <div>
-        <h3>Logged Exercises</h3>
-        <br></br>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-              <th>Description</th>
-              <th>Duration</th>
-              <th>Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.exerciseList() }
-          </tbody>
-        </table>
+      <div class="container">
+        <h2>Exercises</h2>
+        <ul class="responsive-table">
+          <li class="table-header">
+            <div class="col col-2">Name</div>
+            <div class="col col-3">Description</div>
+            <div class="col col-2">Duration</div>
+            <div class="col col-2">Date</div>
+            <div class="col col-2">Actions</div>
+          </li>
+
+          {this.exerciseList()}
+        </ul>
       </div>
     )
   }
